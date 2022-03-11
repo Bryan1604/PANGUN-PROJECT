c616b9ae4754f2c6df9db982b9bb3838908c997e
Luongvu <luongvuhs@gmail.com>
2022-03-11 16:18:32 +0700

    changfing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>PANGUN</title>
+        <title>PANGUN </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -54,7 +54,6 @@
                               color: pink;
                               border: none;
                               font-weight: 1500; 
-                              text-align: center;
                                ">
                       <strong> PANGUN</strong>
                     </h1>
